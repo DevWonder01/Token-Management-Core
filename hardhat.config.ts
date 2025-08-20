ef1d98add92c5ca6dc6e8258c5e5fe8c0d346cae
ArcBase <taiwoadebayo4040@gmail.com>
2025-08-20 18:26:44 +0000

    init : enviroment setup

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -40,6 +40,16 @@ const config: HardhatUserConfig = {
   },
   solidity: {
     compilers: [
+        {
+        version: "0.8.28",
+        settings: { 
+          viaIR: true,
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
+        },
+      },
       {
         version: "0.8.20",
         settings: { 
